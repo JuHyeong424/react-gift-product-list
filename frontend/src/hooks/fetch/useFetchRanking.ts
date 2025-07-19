52c45dbfc6e0915b49fdc2ae9d42dc40f41727c3
JuHyeong <wkdwngud963@gmail.com>
2025-07-19 13:30:19 +0900

    fix: 타입 이동 완료

--- frontend/src/hooks/fetch/useFetchRanking.ts
+++ frontend/src/hooks/fetch/useFetchRanking.ts
@@ -1,6 +1,21 @@
 import { RANKING_URL } from '@/api/api.ts';
 import useFetchData from '@/hooks/fetch/useFetchData.ts';
-import type { Product } from '@/types/productData';
+
+interface Product {
+  id: string | number;
+  name: string;
+  price: {
+    basicPrice: string | number;
+    sellingPrice: string;
+    discountRate: string | number;
+  };
+  imageURL: string;
+  brandInfo: {
+    id: string | number;
+    name: string;
+    imageURL: string;
+  };
+}
 
 export default function useFetchRanking(targetType: string, rankType: string) {
   const {
